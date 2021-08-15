e186f97189a78875f91c6b1acfa4af4533922946
v587yuan <shily1821@163.com>
2021-08-15 21:20:42 +0800

    mx6ull_alientek_emmc uboot ok

---
+++ mx6ull_14x14_evk_emmc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
+make ARCH=arm CROSS_CONPILE=arm-linux-gnueabihf- mx6ull_14x14_evk_emmc_defconfig
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- V=1 -j1
+
